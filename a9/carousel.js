6cf9b8dabf7ed8b04a23075d173e7bbbc05abf68
Garret G <g.grant92@gmail.com>
2023-05-19 06:13:33 -0700

    a9 complete

--- a9/carousel.js
+++ a9/carousel.js
@@ -1,5 +1,29 @@
 "use strict";
 
 $(document).ready( () => {
-    
+    $("a").click( e => {
+        // prevent default action when clicked
+        e.preventDefault();
+        
+        // Get the url of the clicked book
+        const aTag = e.currentTarget;
+        const imgUrl = aTag.getAttribute("href");
+       
+        // Assign and animate the new image to the main image
+        const mainImg = $("#image");
+        mainImg.animate({
+            opacity: 0,
+            marginLeft: "-=205"
+        },
+        {
+            duration:1000,
+            complete: ()=>{
+                mainImg.attr("src", imgUrl);
+                mainImg.animate({
+                    opacity: 1,
+                    marginLeft: "+=205"
+                }, 1000);
+            }});
+
+    });
 });
