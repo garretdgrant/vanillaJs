ae10eff69a4df2afd3de67459d734bdb08d9bc79
Garret G <g.grant92@gmail.com>
2023-05-20 12:34:30 -0700

    add carousel functionality

--- a9/carousel.js
+++ a9/carousel.js
@@ -1,7 +1,8 @@
 "use strict";
 
-$(document).ready( () => {
-    $("a").click( e => {
+var shiftLeft = 0;
+
+const changeMainImage = e => {
         // prevent default action when clicked
         e.preventDefault();
         
@@ -17,5 +18,34 @@ $(document).ready( () => {
                 mainImg.attr("src", imgUrl);
                 mainImg.animate({opacity: 1, marginLeft: "+=205"}, 1000);
             }});
-    });
+}
+
+const handleLeftClick = (e, shiftCheck) => {
+    e.preventDefault();
+    if(shiftCheck.shift == 4){
+        return;
+    }
+    shiftCheck.shift += 1;
+    // Grab all the a tags and shift left
+    $("a").animate(
+        {marginLeft: "-=205"}
+    )
+}
+
+const handleRightClick = (e, shiftCheck) => {
+    e.preventDefault();
+    if (!shiftCheck.shift){ // dont want to allow rShift if lShift has not occured
+        return;
+    }
+    shiftCheck.shift -= 1;
+    $("a").animate(
+        {marginLeft: "+=205"}
+    )
+}
+
+$(document).ready( () => {
+    const shiftCheck = {'shift': 0};
+    $("a").click((e) => changeMainImage(e))
+    $("#left_button").click( e => handleLeftClick(e, shiftCheck));
+    $("#right_button").click( e => handleRightClick(e, shiftCheck));
 });
